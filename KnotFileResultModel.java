df616436b7c53f37ac89f9cf78b11f61388badae
kkjdrummer99 <kkjdrummer@kaist.ac.kr>
2013-01-20 06:26:39 -0800

    Create KnotFileResultModel.java

---
+++ KnotFileResultModel.java
@@ -0,0 +1,38 @@
+package com.kse.model;
+
+
+public class KnotFileResultModel {
+  
+	private String prx;
+	private String pf;
+	private String out;
+	
+	
+	
+	public String getPrx() {
+		return prx;
+	}
+	public void setPrx(String prx) {
+		this.prx = prx;
+	}
+	public String getPf() {
+		return pf;
+	}
+	public void setPf(String pf) {
+		this.pf = pf;
+	}
+	public String getOut() {
+		return out;
+	}
+	public void setOut(String out) {
+		this.out = out;
+	}
+	
+	
+	
+	
+	
+
+	
+
+}
