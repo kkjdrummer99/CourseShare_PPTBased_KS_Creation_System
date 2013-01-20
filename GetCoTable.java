ed940960ecc307c7c6efe095ff530d647546f791
kkjdrummer99 <kkjdrummer@kaist.ac.kr>
2013-01-20 06:41:27 -0800

    Create GetCoTable.java

---
+++ GetCoTable.java
@@ -0,0 +1,11 @@
+package com.kse.knot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public interface GetCoTable {
+  public double[][] getCoTable(ArrayList<HashMap<String,Integer>> compareArray, ArrayList<String> selectedListTermArray);
+}
+
+
+	
