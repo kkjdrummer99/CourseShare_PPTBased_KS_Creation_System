ccc1c1e4389b6325b1f239852f6917f9c74eb69f
kkjdrummer99 <kkjdrummer@kaist.ac.kr>
2013-01-20 06:42:11 -0800

    Create EdgeModel.java

---
+++ EdgeModel.java
@@ -0,0 +1,32 @@
+package com.kse.model;
+
+public class EdgeModel {
+  
+	private LinkModel linkModel;
+	private int from;
+	private int to;
+	
+	
+	
+	public LinkModel getLinkModel() {
+		return linkModel;
+	}
+	public void setLinkModel(LinkModel linkModel) {
+		this.linkModel = linkModel;
+	}
+	public int getFrom() {
+		return from;
+	}
+	public void setFrom(int from) {
+		this.from = from;
+	}
+	public int getTo() {
+		return to;
+	}
+	public void setTo(int to) {
+		this.to = to;
+	}
+	
+	
+
+}
