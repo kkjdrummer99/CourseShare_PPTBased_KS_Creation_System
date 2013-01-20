4a61be9a570c53e38c06acc7c26b3d53bb924ca5
kkjdrummer99 <kkjdrummer@kaist.ac.kr>
2013-01-20 06:42:41 -0800

    Create KnotInputModel.java

---
+++ KnotInputModel.java
@@ -0,0 +1,33 @@
+package com.kse.model;
+
+public class KnotInputModel {
+  
+	private int rowCount;
+	
+	private double[][] coTable;
+	
+	
+
+	public int getRowCount() {
+		return rowCount;
+	}
+
+	public void setRowCount(int rowCount) {
+		this.rowCount = rowCount;
+	}
+
+	public double[][] getCoTable() {
+		return coTable;
+	}
+
+	public void setCoTable(double[][] coTable) {
+		this.coTable = coTable;
+	}
+	
+	
+	
+	
+	
+	
+
+}
