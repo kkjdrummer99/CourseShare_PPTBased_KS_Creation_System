1be3ef39b1866a9bc1d0f66757482d3d9c4e642d
kkjdrummer99 <kkjdrummer@kaist.ac.kr>
2013-01-20 06:52:04 -0800

    Create FileTypeFilter.java

---
+++ FileTypeFilter.java
@@ -0,0 +1,33 @@
+package com.kse.util;
+
+import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
+
+public class FileTypeFilter extends FileFilter {
+  
+	private String extension;
+    private String description;
+ 
+    public FileTypeFilter(String extension, String description) {
+        this.extension = extension;
+        this.description = description;
+    }
+    
+    
+
+	@Override
+	public boolean accept(File file) {
+		 if (file.isDirectory()) {
+	            return true;
+	        }
+	        return file.getName().endsWith(extension);
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return description + String.format(" (*%s)", extension);
+	}
+
+}
